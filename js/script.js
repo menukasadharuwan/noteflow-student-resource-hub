fde11e58d77277c393a3391e422e6b462f0343d8
menukasadharuwan <menuka2380@gmail.com>
2026-07-24 13:33:35 +0530

    Create navigation bar with responsive

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const nav_menu_icon = document.getElementById("menu-icon");
+const navbar_links = document.getElementById("navbar-links");
+
+nav_menu_icon.addEventListener("click",()=>{
+    
+    if(navbar_links.style.display == "block"){
+        navbar_links.style.display = "none";
+        nav_menu_icon.src = "../images/icons/menu.svg";
+    }else{
+        navbar_links.style.display = "block";
+        nav_menu_icon.src = "../images/icons/Cansal.svg";
+        nav_menu_icon.style.width = "35px"
+        
+    }
+})
